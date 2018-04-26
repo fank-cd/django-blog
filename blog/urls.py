bd3c5da900408a0e828c65ae7ee2bdead8c27ff2
daye <2464512446@qq.com>
2018-04-26 17:31:35 +0800

    add time date

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$',
         views.blog_detail, name='blog_detail'),
     url(r'^$',views.blog_list,name="blog_list"),
-    url(r'^tags/(?P<pk>[0-9]+)/$',views.blog_with_type,name="blog_with_type")
+    url(r'^tags/(?P<pk>[0-9]+)/$',views.blog_with_type,name="blog_with_type"),
+    url(r'^date/(?P<year>[0-9]+)/(?P<month>[0-9]+)$', views.blog_with_date, name="blog_with_date"),
 ]
